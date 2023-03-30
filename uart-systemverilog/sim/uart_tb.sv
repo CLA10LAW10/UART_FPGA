63f9060b852c58cede5aa45ac2c032e5e08fd32d
Clayton Lawton <cla10law10@gmail.com>
2023-03-30 09:03:47 -0700

    VHDL Working, Corrected Baud Rate

--- uart-systemverilog/sim/uart_tb.sv
+++ uart-systemverilog/sim/uart_tb.sv
@@ -4,7 +4,7 @@ module uart_tb();
 
   parameter DBIT = 8; // # data bits
   parameter SB_TICK = 1; // # 16 ticks for 1 stop bit
-  parameter FIFO_W = ; // # addr bits of FIFO
+  parameter FIFO_W = 0; // # addr bits of FIFO
 
   reg clk;
   reg reset;
@@ -15,5 +15,5 @@ module uart_tb();
   wire  tx;
   reg rx;
 
-  uart #(.DBIT(DBIT), .SB_TICK(SB_TICK),.FIFO_W(FIFO_W)) uart_uut (.*);
+  uart_top #(.DBIT(DBIT), .SB_TICK(SB_TICK),.FIFO_W(FIFO_W)) uart_uut (.*);
 endmodule
--- uart-systemverilog/src/uart_top.sv
+++ uart-systemverilog/src/uart_top.sv
@@ -1,10 +1,14 @@
+// baud rate = clock rate/16/(dvsr+1)
 module uart_top
-  #(parameter  FIFO_DEPTH_BIT = 0),  // # addr bits of FIFO
-  parameter DBIT = 8,               // # Data bits
-  parameter SB_TICK = 16,           // # ticks for stop bits
+  #(
+     parameter DBIT = 8,               // # Data bits
+     parameter SB_TICK = 16,           // # ticks for stop bits
+     parameter  FIFO_DEPTH_BIT = 0
+   )  // # addr bits of FIFO
    (
      input  logic clk,
      input  logic reset,
+     input  logic [3:0] sw,
      // slot interface
      output logic [3:0] led,
      output logic led_r,
@@ -16,30 +20,29 @@ module uart_top
    );
 
   // signal declaration
-  logic wr_en, wr_uart, rd_uart, wr_dvsr ;
+  logic wr_en, wr_uart, rd_uart, wr_dvsr;
   logic tx_full, rx_empty;
-  logic [10:0] dvsr_reg;
   logic [7:0] r_data;
   logic [7:0] w_data;
   logic [10:0] dvsr_reg;
 
-  parameter [10:0] dvsr 11'd68; //  dvsr = SYS_CLK_FREQ *1000000 / 16 / baud - 1; roundup(125 * 1_000_000)/16/(115200-1);
+  parameter [10:0] dvsr = 11'd68; //  dvsr = SYS_CLK_FREQ *1000000 / 16 / baud - 1; roundup(125 * 1_000_000)/16/(115200-1);
 
   // body
   // instantiate uart
   uart #(.DBIT(DBIT), .SB_TICK(SB_TICK), .FIFO_W(FIFO_DEPTH_BIT)) uart_unit
        (.*, .dvsr(dvsr_reg), .w_data(w_data));
 
-  // dvsr register
-//   always_ff @(posedge clk, posedge reset)
-//     if (reset)
-//       dvsr_reg <= 0;
-//     else
-//       if (wr_dvsr)
-//         dvsr_reg <= wr_data[10:0];
+  // // dvsr register
+  always_ff @(posedge clk, posedge reset)
+    if (reset)
+      dvsr_reg <= 0;
+    else
+      if (wr_uart)
+        dvsr_reg <= dvsr;
 
-assign led = r_data[3:0];
-assign wr_uart = 1'b1;
-assign rd_uart = 1'b1;
+  assign led = r_data[3:0];
+  assign wr_uart = sw[0];
+  assign rd_uart = 1'b1;
 
 endmodule
